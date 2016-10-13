d338b4200c5e8009d6df58aed3175400c96256ec
Brian Doyal <brian.doyal@gmail.com>
2016-10-13 02:51:25 -0500

    Add comments

--- main.js
+++ main.js
@@ -4,36 +4,54 @@ var Game = require('./lib/game.js');
 
 var game = new Game();
 
+/**
+ * Renders the main screen, including the word being guessed and any player
+ * info (i.e. score, wrong letters guessed, guesses remaining)
+ */
 var render = function() {
     // "clear" the console
     console.log('\033[2J');
     // Create ascii art based on the word
-    console.log(figlet.textSync(game.word.render()));
+    console.log(figlet.textSync(game.word.render().replace(' ', '    ')));
+    console.log('\n\n\n\n');
+    console.log(`Score: ${game.score}`);
     // Print the previously guessed letters
     console.log(`Incorrect guesses: [ ${[...game.guesses].join(', ')} ]`);
     // Print the score and number of guesses remaining
-    console.log(`Score: ${game.score}, Guesses: ${game.tries})`);
+    console.log(`Remaining guesses: ${game.tries}`);
 };
 
+/**
+ * Determines if the game is over, renders the main screen, messages the user
+ * if the game has ended, then prompts the user as appropriate
+ */
 var menu = function() {
-    render();
     if (game.finished() && game.tries > 0) {
+    	render();
         console.log('Congratulations, you get a point!')
         restart();
     } else if (game.finished() && game.tries <= 0) {
+    	game.word.reveal();
+    	render();
         console.log('Sorry, you have run out of guesses.');
         restart();
     } else {
-        guess();
+    	render();
+        prompt();
     }
 };
 
-var guess = function() {
+/**
+ * Prompts the user for a letter to guess
+ */
+var prompt = function() {
     inquirer
         .prompt([{
             message: 'Guess a letter',
             type: 'input',
             name: 'letter',
+            // Validates the user input is one letter,
+            // between a and z and hasn't already been guessed
             validate: function(input) {
                 if (input.length > 1) {
                     return 'Guess one letter at a time';
@@ -45,16 +63,21 @@ var guess = function() {
                     return true;
                 }
             },
+            // Makes sure the letter is lower case before checking it
             filter: function(input) {
                 return input.toLowerCase();
             }
         }])
         .then(function(answers) {
+        	// submits the guess to the game object
             game.guess(answers.letter);
             menu();
         });
 };
 
+/**
+ * Prompts the user if they would like to start a new game
+ */
 var restart = function() {
     inquirer
         .prompt([{
